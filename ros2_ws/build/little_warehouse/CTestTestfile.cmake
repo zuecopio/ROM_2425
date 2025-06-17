422f89042a1b794aac0115f229e7c68d2278d7fb
Marcos Belda Martínez <mbelmar@etsinf.upv.es>
2025-06-17 16:59:06 +0200

    New update

--- ros2_ws/build/little_warehouse/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alumno.upv.es.mbelmar/W/GIIROB/G3/ROM/myGitHub/ROM_2425/ros2_ws/src/little_warehouse
-# Build directory: /home/alumno.upv.es.mbelmar/W/GIIROB/G3/ROM/myGitHub/ROM_2425/ros2_ws/build/little_warehouse
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
